3d5af13fc526522bc30f2893bbf8a0705f9da20e
MacOS <macos@MacOSs-MacBook-Pro.local>
2024-07-27 15:58:29 +0700

    bandocoban

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,17 @@
-import 'package:firebase_ui_auth/firebase_ui_auth.dart'; // new
+import 'package:basemap/mapbase.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';               // new
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';                 // new
-
-import 'app_state.dart';                                 // new
-import 'mapbase.dart';
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(ChangeNotifierProvider(
-    create: (context) => ApplicationState(),
-    builder: ((context, child) => const MyApp()),
-  ));
+  runApp(const MyApp());
 }
-
-final _router = GoRouter(routes: [
-  GoRoute(path: '/', builder: (context, state) => const MyHomePage(), routes: [
-    GoRoute(
-      path: 'sign-in',
-      builder: (context, state) {
-        return SignInScreen(
-          actions: [
-            ForgotPasswordAction(((context, email) {
-              final uri = Uri(
-                path: '/sign-in/forgot-password',
-                queryParameters: <String, String?>{
-                  'email': email,
-                },
-              );
-              context.push(uri.toString());
-            })),
-            AuthStateChangeAction(((context, state) {
-              final user = switch (state) {
-                SigningIn state => state.user,
-                UserCreated state => state.credential.user,
-                _ => null
-              };
-              if (user == null) {
-                return;
-              }
-              if (state is UserCreated) {
-                user.updateDisplayName(user.email!.split('@')[0]);
-              }
-              if (!user.emailVerified) {
-                user.sendEmailVerification();
-                snackBar = const SnackBar(
-                  content: Text(
-                      'Please check your email to verify your email address'),
-                );
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              }
-              context.pushReplacement('/');
-            })),
-          ],
-        );
-      },
-      routes: [
-        GoRoute(
-          path: 'forgot-password',
-          builder: (context, state) {
-            final arguments = state.uri.queryParameters;
-            return ForgotPasswordScreen(
-              email: arguments['email'],
-              headerMaxExtent: 200,
-            );
-          },
-        ),
-      ],
-    ),
-    GoRoute(
-        path: 'profile',
-        builder: (context, state) {
-          return ProfileScreen(
-            providers: const [],
-            actions: [
-              SignedOutAction((context) {
-                context.pushReplacement('/');
-              }),
-            ],
-          );
-        })
-  ])
-]);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'basemap',
       theme: ThemeData(
@@ -104,7 +26,8 @@ class MyApp extends StatelessWidget {
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      routerConfig: _router,
+      home: const MyHomePage(),
     );
+    
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
@@ -14,7 +13,6 @@ import geolocator_apple
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
