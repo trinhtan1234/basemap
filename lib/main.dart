2f330e26c2e05215ccc2cd64a5bc8b1990693482
MacOS <macos@MacOSs-MacBook-Pro.local>
2024-07-29 23:38:13 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:basemap/mapbase.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -28,6 +30,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(),
     );
-    
   }
 }
